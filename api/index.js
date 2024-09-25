4476c1177018d5b707537dffb814d50ef3f75a3e
Mohd Faiz Ansari <109471065+faizansari45@users.noreply.github.com>
2024-09-25 19:35:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,22 +3,15 @@ const dbConnection = require("./dbConnection");
 const app = Express();
 const cors = require("cors");
 require("dotenv").config();
-
-// Vercel doesn't use a fixed port like traditional apps.
-// You don't need to define PORT or listen on it.
+const PORT = process.env.PORT || 3000;
 app.use(Express.json());
 app.use(cors());
-
 app.get("/", (req, res) => {
   res.json({"message": "Hello friend."});
 });
-
-// Use the routes from your router
 app.use("/api/v1", require("./router/routes"));
 
-// This export is necessary for Vercel
-module.exports = app;
-
-// Optionally, if you want to include the database connection
-// You can use a function to establish a connection when the app is initialized.
-dbConnection(); // Call your DB connection function
+app.listen(PORT, () => {
+  dbConnection();
+  console.log(My app is listening on port ${PORT});
+});
