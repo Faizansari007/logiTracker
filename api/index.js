3fffcf7c7235b329f9c3c5c32b15261b7558b6a1
amanchandra100 <aman123chandra@gmail.com>
2024-09-25 19:58:20 +0530

    Updating index js

--- api/index.js
+++ api/index.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 3000;
 app.use(Express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-  res.json({"message": "Hello friend."});
+  res.send("<h1>Wellcome to Logitrack Api</h1>");
 });
 app.use("/api/v1", require("./router/routes"));
 
 app.listen(PORT, () => {
   dbConnection();
-  console.log(My app is listening on port ${PORT});
+  console.log(`My app is listening on port ${PORT}`);
 });
