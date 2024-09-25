24a44eac173591b5f3a02ec4f5a044419394de65
amanchandra100 <aman123chandra@gmail.com>
2024-09-26 00:28:40 +0530

    fixing cors issue

--- api/index.js
+++ api/index.js
@@ -8,9 +8,15 @@ app.use(Express.json());
 // app.use(cors())
 app.use(cors(
   {
-    origin: ["https://logi-tracker-ihzh.vercel.app"]
+    origin: ['https://logi-tracker-ihzh.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials if needed
   }
 ));
+
+app.options('*', cors()); // Allow preflight requests for all routes
+
+
 app.get("/", (req, res) => {
   res.send("<h1>Wellcome to Logitrack Api</h1>");
 });
